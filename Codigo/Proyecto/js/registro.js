62899bac349f3913b58cce48b0618a0f34802a7a
Joyce Sarmiento <joyasarm@espol.edu.ec>
2015-12-21 09:20:27 -0500

    vv

--- Codigo/Proyecto/js/registro.js
+++ Codigo/Proyecto/js/registro.js
@@ -3,4 +3,23 @@ function iniciarSesion(){
     btnSign.setAttribute("data-toggle", "modal");
     btnSign.setAttribute("data-target", "#ejemMod");
 }
-window.addEventListener("load", iniciarSesion, false);
+
+function validacionCampos(){        
+    if(nuName.getAttribute("placeholder") == "Username" || nuName.getAttribute("placeholder") == "" ){
+        nuName.setAttribute("placeholder","Write a username");
+        nuName.setAttribute("color",red);
+    }
+    if(nuemail.getAttribute("placeholder")="email" || nuemail.getAttribute("placeholder")=""){
+        nuemail.setAttribute("placeholder","Write a username");
+        nuemail.setAttribute("color",red);
+    }
+    if(nuPassword.getAttribute("placeholder")="New Password" || nuPassword.getAttribute("placeholder")=""){
+        nuPassword.setAttribute("placeholder","Write a username");
+        nuPassword.setAttribute("color",red);
+    }
+    if(nuName.getAttribute("placeholder") !="" && nuemail.getAttribute("placeholder")!="" && nuPassword.getAttribute("placeholder")!=""){
+        document.getElementById("myModal").setAttribute("data-dismiss",myModal);
+        alert("Proceed to log in")
+    }
+}
+window.addEventListener("load", validacionCampos, false);
